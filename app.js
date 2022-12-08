d00afe77a3c4373e2d924dd8975e07cbf10b35bd
gameisboring <daum128@naver.com>
2022-12-08 15:26:15 +0900

    자잘한 내용 수정

--- app.js
+++ app.js
@@ -20,20 +20,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-const cspOptions = {
-  directives: {
-    ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-    /** Remove annotation if using jquery CDN
-    'script-src': ["'self'", 'data:', 'http://code.jquery.com/'],*/
-  },
-}
-
-app.use(
-  helmet({
-    contentSecurityPolicy: cspOptions,
-    crossOriginEmbedderPolicy: false,
-  })
-)
+app.use(helmet.hidePoweredBy())
 
 app.use('/', indexRouter)
 
